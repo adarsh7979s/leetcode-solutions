6cac7a037ab8f319acaa13f69c6485d5f0d49e95
Adarsh Sharma <160575373+adarsh7979s@users.noreply.github.com>
2026-09-19 18:43:06 +0530

    Time: 27 ms (90.43%), Space: 20.2 MB (73.74%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -16,14 +16,31 @@ class Solution(object):
 
         # return ans
 
-        n = len(nums)
-        ans = [1] * n
+        # n = len(nums)
+        # ans = [1] * n
 
-        for i in range(1, n):
-            ans[i] = ans[i-1] * nums[i -1]
+        # for i in range(1, n):
+        #     ans[i] = ans[i-1] * nums[i -1]
 
-        rightproduct = 1
-        for i in range(n-1, -1, -1):
-            ans[i] = ans[i] * rightproduct 
-            rightproduct = rightproduct * nums[i]
-        return ans
+        # rightproduct = 1
+        # for i in range(n-1, -1, -1):
+        #     ans[i] = ans[i] * rightproduct 
+        #     rightproduct = rightproduct * nums[i]
+            
+        # return ans
+
+
+
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix = prefix * nums[i]
+
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] = res[i] * postfix
+            postfix = postfix * nums[i]
+
+        return res
