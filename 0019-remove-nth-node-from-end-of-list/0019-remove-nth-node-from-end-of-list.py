8fddc678112ee5c6fa158fb1029a20057a00ab8c
Adarsh Sharma <160575373+adarsh7979s@users.noreply.github.com>
2026-09-26 02:14:50 +0530

    Time: 0 ms (100%), Space: 12.4 MB (28.7%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,30 +1,52 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# # Definition for singly-linked list.
+# # class ListNode(object):
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution(object):
+#     def removeNthFromEnd(self, head, n):
+#         """
+#         :type head: Optional[ListNode]
+#         :type n: int
+#         :rtype: Optional[ListNode]
+#         """
+#         length = 0
+#         current = head
+#         while current != None:
+#             length +=1
+#             current = current.next
+#         k = length - n
+
+#         if k == 0:
+#             return head.next
+
+
+#         current = head
+#         for i in range(k-1):
+           
+#             current = current.next
+#         current.next = current.next.next
+
+#         return head
+
 class Solution(object):
     def removeNthFromEnd(self, head, n):
-        """
-        :type head: Optional[ListNode]
-        :type n: int
-        :rtype: Optional[ListNode]
-        """
-        length = 0
-        current = head
-        while current != None:
-            length +=1
-            current = current.next
-        k = length - n
+        dummy = ListNode(0)
+        dummy.next = head
 
-        if k == 0:
-            return head.next
+        slow = dummy
+        fast = dummy
 
-            
-        current = head
-        for i in range(k-1):
-           
-            current = current.next
-        current.next = current.next.next
+        # Move fast n steps ahead
+        for i in range(n):
+            fast = fast.next
 
-        return head
+        # Move both until fast reaches the end
+        while fast.next is not None:
+            slow = slow.next
+            fast = fast.next
+
+        # Remove the target node
+        slow.next = slow.next.next
+
+        return dummy.next
